6da50f0a8271dc553348f7156e4bd0d8a2f46415
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-02-23 19:04:44 +0530

    cool

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 export const __is_prod__ = false; //process.env.NODE_ENV === "production";
-export const CLIENT_URL = __is_prod__
-  ? "https://klu.vercel.app"
-  : "http://localhost:3000";
+export const CLIENT_URL =
+  __is_prod__ || true ? "https://klu.vercel.app" : "http://localhost:3000";
 export const API_URL = __is_prod__
   ? "https://klu.up.railway.app"
   : "http://localhost:4001";
